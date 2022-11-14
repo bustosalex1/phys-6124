0431d57bd6add34e3908384620743c138640c79e
Alex Bustos <bustosalex1@gmail.com>
2022-11-14 12:53:09 -0500

    bruh

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
+import DropdownMenu from './components/DropdownMenu'
 import { Projection } from './components/Projection'
+import { objectProjectionOptions } from './components/ProjectionOptions'
 
 const App = () => {
+    const [selectedItem, setSelectedItem] = useState(objectProjectionOptions[0])
+
     return (
         <div className="bg-slate-700 h-screen flex items-center justify-start flex-col space-y-10 p-20">
             <div className="text-center text-4xl text-white font-semibold font-serif drop-shadow-xl">
                 PHYS 6124 Final Project
             </div>
-            <div className="p-20 bg-white rounded-md border-1 border-black drop-shadow-xl">
-                <Projection projectionType="Airy Minimum" />
+            <div className="flex items-center justify-center flex-row space-x-10">
+                <DropdownMenu
+                    setSelectedItem={setSelectedItem}
+                    selectedItem={selectedItem}
+                />
+
+                <div className="p-20 bg-white rounded-md border-1 border-black drop-shadow-xl">
+                    <Projection projectionType={selectedItem.name} />
+                </div>
             </div>
         </div>
     )
--- src/components/ProjectionOptions.ts
+++ src/components/ProjectionOptions.ts
@@ -1,10 +1,18 @@
 import * as d3 from 'd3'
 import * as d3GeoProjection from 'd3-geo-projection'
 
-export const projectionOptions: {[name: string]: any} = {
-    'Airy Minimum': d3GeoProjection.geoAiry,
+export const projectionOptions: { [name: string]: any } = {
+    'Airy Minimum Error': d3GeoProjection.geoAiry,
     'Mercator': d3.geoMercator,
-    'albers': d3.geoAlbers,
-    'equirectangular': d3.geoEquirectangular,
-    'azimuthal equidistant': d3.geoAzimuthalEquidistant
+    'Albers': d3.geoAlbers,
+    'Equirectangular': d3.geoEquirectangular,
+    'Azimuthal Equidistant': d3.geoAzimuthalEquidistant,
 }
+
+export const objectProjectionOptions = [
+    { name: 'Airy Minimum Error', projection: d3GeoProjection.geoAiry },
+    { name: 'Mercator', projection: d3.geoMercator },
+    { name: 'Albers', projection: d3.geoAlbers },
+    { name: 'Equirectangular', projection: d3.geoEquirectangular },
+    { name: 'Azimuthal Equidistant', projection: d3.geoAzimuthalEquidistant },
+]
